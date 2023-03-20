ac45f2fb4ff6a381970f2afbc3729a41144d5f0d
sathsara95 <sgwijewardana@gmail.com>
2023-03-20 22:44:09 +0530

    add controllers

--- demo/src/main/java/com/example/demo/controller/AuthenticationController.java
+++ demo/src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -1,4 +1,43 @@
 package com.example.demo.controller;
 
+
+
+import com.example.demo.config.JwtUtils;
+import com.example.demo.dao.UserDao;
+import com.example.demo.dto.AuthenticationRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api/v1/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
+
+    private final AuthenticationManager authenticationManage;
+    private UserDao userDao;
+    private final JwtUtils jwtUtils;
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<String> autheticate(
+            @RequestBody AuthenticationRequest request
+    ){
+        authenticationManage.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        final UserDetails userDetails= userDao.findUserByEmail(request.getEmail());
+        if(userDetails!=null){
+            return ResponseEntity.ok(jwtUtils.generateToken(userDetails));
+        }else {
+            return ResponseEntity.status(400).body("some error has occored");
+        }
+    }
+
 }
--- demo/src/main/java/com/example/demo/controller/UserController.java
+++ demo/src/main/java/com/example/demo/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.example.demo.controller;
 
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/api/v1/user")
 public class UserController {
+    @GetMapping()
+    public String getMessage(){
+        return"Springboot user controller work!";
+    }
+
+    @PostMapping()
+    public String setMessage(@RequestBody String message){
+        return "Entered message: "+message;
+    }
 }
